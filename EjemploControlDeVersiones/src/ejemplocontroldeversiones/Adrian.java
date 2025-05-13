16c1eb617dbbc854e389a88e7980d82988f46a43
Romero0112 <adri0986127348@gmail.com>
2025-05-13 14:53:19 -0500

    Creacion de clase Adrian

---
+++ EjemploControlDeVersiones/src/ejemplocontroldeversiones/Adrian.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplocontroldeversiones;
+
+/**
+ *
+ * @author adri0
+ */
+public class Adrian {
+    private String lenguajeFavorito;
+    private int edad;
+
+    // Constructor
+    public Adrian(String lenguajeFavorito, int edad) {
+        this.lenguajeFavorito = lenguajeFavorito;
+        this.edad = edad;
+    }
+
+    // Método para presentarse
+    public void presentarse() {
+        System.out.println("👋 ¡Hola! Soy Adrian, tengo " + edad + " años y mi lenguaje favorito es " + lenguajeFavorito + ".");
+    }
+
+    // Método principal para probar la clase
+    public static void main(String[] args) {
+        Adrian yo = new Adrian("php", 21);
+        yo.presentarse();
+    }
+}
