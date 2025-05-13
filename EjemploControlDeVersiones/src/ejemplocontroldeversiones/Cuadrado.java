0106124744ca6fb6695137e31c172bc16b735d91
Kevin Peñafiel <alexpenafiel27@gmail.com>
2025-05-13 15:04:26 -0500

    "commit saludo"

--- EjemploControlDeVersiones/src/ejemplocontroldeversiones/Cuadrado.java
+++ EjemploControlDeVersiones/src/ejemplocontroldeversiones/Cuadrado.java
@@ -13,5 +13,6 @@ public class Cuadrado {
     public static void Prueba(){
         System.out.println("esta es una prueba");
         System.out.println("Esta es mia jaja AlexaNDER");
+        System.out.println("hola");
     }
 }
