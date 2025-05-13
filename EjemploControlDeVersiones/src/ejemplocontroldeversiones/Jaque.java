130dee500ca1937c674c11fe09d88f9f5e89df10
ArteMisa82 <anavero162@gmail.com>
2025-05-13 15:00:21 -0500

    commit realizo constructor

---
+++ EjemploControlDeVersiones/src/ejemplocontroldeversiones/Jaque.java
@@ -0,0 +1,21 @@
+package ejemplocontroldeversiones;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Jaque {
+    
+    String nombre, apellido;
+
+    public Jaque(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    
+}
